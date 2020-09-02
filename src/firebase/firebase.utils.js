d7d2a9e4e2eefd7446c36fb3b5cd5aa00f22c5db
Dibyadeep Bhattacharya <dibyadeepbhattacharyya1998@gmail.com>
2020-09-02 15:52:37 +0530

    Integreted Firebase, created the sign-in page with the sign-in componentand sign-in-with-google component

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyD_RCaZOXGr5W2v0LWSr-QoZvchnlpT6wY",
+    authDomain: "e-commerce-db-b3586.firebaseapp.com",
+    databaseURL: "https://e-commerce-db-b3586.firebaseio.com",
+    projectId: "e-commerce-db-b3586",
+    storageBucket: "e-commerce-db-b3586.appspot.com",
+    messagingSenderId: "74931519886",
+    appId: "1:74931519886:web:9e32d089e2e54888a1e4df",
+    measurementId: "G-D9JSFDWDT0"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({prompt: 'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export default firebase;
